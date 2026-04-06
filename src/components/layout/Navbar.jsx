b7cc5577c5459dda07d164c8f22ba4f0fa7c82b2
Magret <belloabiodun17@gmail.com>
2026-04-06 20:25:32 -0230

    Add registration page

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -60,7 +60,7 @@ export default function Navbar() {
 
         {/* Right Side */}
         <div className="hidden md:flex items-center gap-3">
-          {/* {!isAuthenticated ? (
+          {!isAuthenticated ? (
             <>
               <Link
                 to="/login"
@@ -76,7 +76,7 @@ export default function Navbar() {
                 Create Account
               </Link>
             </>
-          ) : ( */}
+          ) : (
             <Link
               to="/me"
               className="flex items-center gap-2 px-4 py-2.5 rounded-xl border border-gray-300 text-black font-medium hover:bg-gray-50 transition"
@@ -84,7 +84,7 @@ export default function Navbar() {
               <User className="w-5 h-5" />
               <span>Account</span>
             </Link>
-          {/* )} */}
+          )}
         </div>
 
         {/* Mobile Menu Button */}
--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,3 +1,120 @@
+import { Link } from "react-router-dom";
+import { Eye, Ticket } from "lucide-react";
+
 export default function Register() {
-    return <div className="text-xl font-semibold">Register Page</div>;
-  }
+  return (
+    <section className="min-h-2 bg-[#f7f7f7] px-4 py-10 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-xl">
+        <div className="rounded-3xl border border-gray-200 bg-white p-6 shadow-sm sm:p-10">
+          <div className="flex flex-col items-center text-center">
+            <div className="mb-4 flex items-center gap-3">
+              <div className="flex h-8 w-8 items-center justify-center rounded-md bg-black">
+                <Ticket className="h-4 w-4 text-white" />
+              </div>
+              <span className="text-2xl font-bold text-black">TicketNest</span>
+            </div>
+
+            <h1 className="text-2xl font-bold tracking-tight text-black sm:text-3xl">
+              Create an account
+            </h1>
+
+            <p className=" text-lg text-gray-500 sm:text-lg">
+              Join TicketNest and start booking amazing events
+            </p>
+          </div>
+
+          <form className="mt-8 space-y-4">
+            <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
+              <div>
+                <label
+                  htmlFor="firstName"
+                  className="mb-3 block text-sm font-semibold text-black"
+                >
+                  First Name
+                </label>
+                <input
+                  id="firstName"
+                  type="text"
+                  placeholder="John"
+                  className="h-10 w-full rounded-sm border border-gray-300 px-4 text-lg text-black outline-none transition placeholder:text-gray-400 focus:border-black"
+                />
+              </div>
+
+              <div>
+                <label
+                  htmlFor="lastName"
+                  className="mb-3 block text-sm font-semibold text-black"
+                >
+                  Last Name
+                </label>
+                <input
+                  id="lastName"
+                  type="text"
+                  placeholder="Doe"
+                  className="h-10 w-full rounded-sm border border-gray-300 px-4 text-lg text-black outline-none transition placeholder:text-gray-400 focus:border-black"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="email"
+                className="mb-3 block text-sm font-semibold text-black"
+              >
+                Email
+              </label>
+              <input
+                id="email"
+                type="email"
+                placeholder="you@example.com"
+                className="h-10 w-full rounded-sm border border-gray-300 px-4 text-lg text-black outline-none transition placeholder:text-gray-400 focus:border-black"
+              />
+            </div>
+
+            <div>
+              <label
+                htmlFor="password"
+                className="mb-3 block text-sm font-semibold text-black"
+              >
+                Password
+              </label>
+
+              <div className="relative">
+                <input
+                  id="password"
+                  type="password"
+                  placeholder="Create a password"
+                  className="h-10 w-full rounded-xl border border-gray-300 px-4 pr-12 text-lg text-black outline-none transition placeholder:text-gray-400 focus:border-black"
+                />
+                <button
+                  type="button"
+                  className="absolute inset-y-0 right-4 flex items-center text-gray-500"
+                >
+                  <Eye className="h-5 w-5" />
+                </button>
+              </div>
+
+              <p className="mt-1 text-xs text-gray-500">
+                Must be at least 8 characters
+              </p>
+            </div>
+
+            <button
+              type="submit"
+              className="h-10 w-full rounded-xl bg-black text-lg font-medium text-white transition hover:bg-gray-800"
+            >
+              Create Account
+            </button>
+          </form>
+
+          <p className="mt-4 text-center text-md text-gray-500">
+            Already have an account?{" "}
+            <Link to="/login" className="font-semibold text-black">
+              Sign in
+            </Link>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
